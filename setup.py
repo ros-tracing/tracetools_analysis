f0bbf76d2133b73a1caa877009ea7b1d3d6ca5fc
Christophe Bedard <fixed-term.christophe.bourquebedard@de.bosch.com>
2019-06-06 09:36:53 +0200

    Setup tests for *analysis and *trace packages

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from setuptools import find_packages
 from setuptools import setup
 
 package_name = 'tracetools_analysis'
@@ -5,7 +6,7 @@ package_name = 'tracetools_analysis'
 setup(
     name=package_name,
     version='0.1.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
     ],
@@ -18,4 +19,5 @@ setup(
             f'process = {package_name}.process:main',
         ],
     },
+    tests_require=['pytest'],
 )

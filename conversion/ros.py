d1ea1bd9bd3f3639ab34a8409cf2b2ac5b297d45
Christophe Bedard <fixed-term.christophe.bourquebedard@de.bosch.com>
2019-06-06 09:20:36 +0200

    Add basic events processing entrypoint

---
+++ conversion/ros.py
@@ -0,0 +1,5 @@
+# Process trace events and create ROS model
+
+def ros_import(events):
+    for event in events:
+        print(f'event: {str(event)}')
---
+++ process.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# Entrypoint/script to process events from a pickle file to build a ROS model
+
+import sys
+import pickle
+from tracetools_analysis.conversion.ros import *
+
+def main(argv=sys.argv):
+    if len(argv) != 2:
+        print('usage: pickle_file')
+        exit(1)
+    
+    pickle_filename = sys.argv[1]
+    with open(pickle_filename, 'rb') as f:
+        events = _get_events_from_pickled_file(f)
+        print(f'imported {len(events)} events')
+        ros_import(events)
+
+def _get_events_from_pickled_file(file):
+    p = pickle.Unpickler(file)
+    events = []
+    while True:
+        try:
+            events.append(p.load())
+        except EOFError as _:
+            break  # we're done
+    return events

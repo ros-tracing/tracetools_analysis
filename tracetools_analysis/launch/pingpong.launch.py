d160f680da37828501751b99939f4ed732814131
Christophe Bedard <fixed-term.christophe.bourquebedard@de.bosch.com>
2019-08-15 14:17:14 +0200

    Add sample pickle file and launchfile for callback_duration analysis

---
+++ tracetools_analysis/launch/pingpong.launch.py
@@ -0,0 +1,39 @@
+# Copyright 2019 Robert Bosch GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Example launch file for a callback duration analysis."""
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from tracetools_launch.action import Trace
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Trace(
+            session_name='pingpong',
+        ),
+        Node(
+            package='tracetools_test',
+            node_executable='test_ping',
+            arguments=['do_more'],
+            output='screen',
+        ),
+        Node(
+            package='tracetools_test',
+            node_executable='test_pong',
+            arguments=['do_more'],
+            output='screen',
+        ),
+    ])

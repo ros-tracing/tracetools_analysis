e757a9855480e0d6236af5324fdb0bdf8e143b7b
Christophe Bedard <bedard.christophe@gmail.com>
2020-03-07 17:33:08 -0500

    Switch from add_subparsers to add_subparsers_on_demand
    Signed-off-by: Christophe Bedard <bedard.christophe@gmail.com>

--- ros2trace_analysis/ros2trace_analysis/command/trace_analysis.py
+++ ros2trace_analysis/ros2trace_analysis/command/trace_analysis.py
@@ -14,9 +14,8 @@
 
 """Module for trace analysis command extension implementation."""
 
-from ros2cli.command import add_subparsers
+from ros2cli.command import add_subparsers_on_demand
 from ros2cli.command import CommandExtension
-from ros2cli.verb import get_verb_extensions
 
 
 class TraceAnalysisCommand(CommandExtension):
@@ -25,9 +24,8 @@ class TraceAnalysisCommand(CommandExtension):
     def add_arguments(self, parser, cli_name):
         self._subparser = parser
         # get verb extensions and let them add their arguments
-        verb_extensions = get_verb_extensions('ros2trace_analysis.verb')
-        add_subparsers(
-            parser, cli_name, '_verb', verb_extensions, required=False)
+        add_subparsers_on_demand(
+            parser, cli_name, '_verb', 'ros2trace_analysis.verb', required=False)
 
     def main(self, *, parser, args):
         if not hasattr(args, '_verb'):

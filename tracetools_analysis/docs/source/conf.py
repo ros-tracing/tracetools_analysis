681d1b546029ccbebf9d1d81992318ca466ca447
Christophe Bedard <bedard.christophe@gmail.com>
2020-06-21 21:39:49 -0400

    Make sidebar larger for tracetools_analysis documentation
    Signed-off-by: Christophe Bedard <bedard.christophe@gmail.com>

--- tracetools_analysis/docs/source/conf.py
+++ tracetools_analysis/docs/source/conf.py
@@ -100,7 +100,9 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'sidebar_width': '260px',
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,

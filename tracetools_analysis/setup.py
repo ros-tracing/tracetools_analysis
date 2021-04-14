dc6a0214c98643309b8c1125d9bbdd82d2243ec1
Christophe Bedard <bedard.christophe@gmail.com>
2021-04-14 14:34:01 -0400

    Revert "Update Python packages to use generate_setuptools_dict"
    This reverts commit b181a68b0a511a78735015d87e1526fcdac297ea.
    
    Signed-off-by: Christophe Bedard <bedard.christophe@gmail.com>

--- tracetools_analysis/setup.py
+++ tracetools_analysis/setup.py
@@ -1,11 +1,13 @@
 import glob
 
-from ament_package.generate_setuptools_dict import generate_setuptools_dict
 from setuptools import find_packages
 from setuptools import setup
 
 package_name = 'tracetools_analysis'
-package_info = generate_setuptools_dict(
+
+setup(
+    name=package_name,
+    version='2.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
@@ -14,7 +16,29 @@ package_info = generate_setuptools_dict(
             ['resource/' + package_name]),
     ],
     install_requires=['setuptools'],
+    maintainer=(
+        'Christophe Bedard, '
+        'Ingo Lütkebohle'
+    ),
+    maintainer_email=(
+        'bedard.christophe@gmail.com, '
+        'ingo.luetkebohle@de.bosch.com'
+    ),
+    author=(
+        'Christophe Bedard, '
+        'Ingo Lütkebohle'
+    ),
+    author_email=(
+        'fixed-term.christophe.bourquebedard@de.bosch.com, '
+        'ingo.luetkebohle@de.bosch.com'
+    ),
+    url='https://gitlab.com/ros-tracing/tracetools_analysis',
     keywords=[],
+    description='Tools for analysing trace data.',
+    long_description=(
+        'This package provides tools for analysing trace data, from '
+        'building a model of the trace data to providing plotting utilities.'
+    ),
     entry_points={
         'console_scripts': [
             f'convert = {package_name}.convert:main',
@@ -24,6 +48,6 @@ package_info = generate_setuptools_dict(
             f'memory_usage = {package_name}.scripts.memory_usage:main',
         ],
     },
+    license='Apache 2.0',
     tests_require=['pytest'],
 )
-setup(**package_info)

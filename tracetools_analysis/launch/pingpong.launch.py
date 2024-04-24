7b12a65f05e0855ed45572dd70f239ff06b158c5
Christophe Bedard <christophe.bedard@apex.ai>
2024-04-24 11:54:18 -0700

    Fix warnings when using mypy>=1.8.0 (#16)
    Signed-off-by: Christophe Bedard <christophe.bedard@apex.ai>
    (cherry picked from commit 760dca9598a04a64b2e1aef0f8e419e8018c79d6)

--- tracetools_analysis/launch/pingpong.launch.py
+++ tracetools_analysis/launch/pingpong.launch.py
@@ -14,13 +14,13 @@
 
 """Example launch file for a callback duration analysis."""
 
-from launch import LaunchDescription
+import launch
 from launch_ros.actions import Node
 from tracetools_launch.action import Trace
 
 
 def generate_launch_description():
-    return LaunchDescription([
+    return launch.LaunchDescription([
         Trace(
             session_name='pingpong',
             events_kernel=[],
